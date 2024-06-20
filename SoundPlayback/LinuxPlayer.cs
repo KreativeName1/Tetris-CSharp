92198ccb6e845f6ef595ba0249b68f736e47907f
= <=>
2024-06-20 15:12:09 +0200

    changes

--- SoundPlayback/LinuxPlayer.cs
+++ SoundPlayback/LinuxPlayer.cs
@@ -1,35 +1,37 @@
-
-using NAudio.Wave;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Tetris.SoundPlayback
 {
-
   public class LinuxPlayer : IAudioPlayer
   {
-    public void PlayMusic(Music music)
-    {
-      throw new NotImplementedException();
-    }
+    public bool MusicOn = true;
 
     public void PlaySound(Sound sound)
     {
-      throw new NotImplementedException();
+      var soundFile = AudioFiles.GetEnumDescription(sound);
+      var command = $"aplay -q {soundFile}";
+      var process = System.Diagnostics.Process.Start("bash", $"-c \"{command}\"");
+      process?.WaitForExit();
     }
 
-    public void TurnOffMusic()
-    {
-      throw new NotImplementedException();
-    }
+    public void TurnOffMusic() => MusicOn = false;
 
-    public void TurnOnMusic()
+    public void TurnOnMusic() => MusicOn = true;
+
+    public async void PlayMusic(Music music)
     {
-      throw new NotImplementedException();
+      var musicFile = AudioFiles.GetEnumDescription(music);
+      var command = $"aplay -q {musicFile}";
+      while (MusicOn)
+      {
+        var process = System.Diagnostics.Process.Start("bash", $"-c \"{command}\"");
+        process?.WaitForExit();
+        await Task.Delay(100);
+      }
+
     }
   }
 }
